9c643a95a048388f0cfc95993a479ff80e971ded
Aman Ahlawat <24404559+ahlawataman@users.noreply.github.com>
2022-01-11 21:18:06 +0530

    Update req.py

--- frontend automation/login_axios/req.py
+++ frontend automation/login_axios/req.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'https://clashofcodes.herokuapp.com/user/signin'
-myobj = '{"email":"ahlawataman3@gmail.com","password":"temporarypass"}'
+myobj = '{"email":"ahlawataman3@gmail.com","password":"----"}'
 head = {'_ga_Q0L0V6P641':'GS1.1.1641448746.2.1.1641449499.0', '_ga':'GA1.1.984749176.1641392013', 'express:sess':'eyJwYXNzcG9ydCI6e319', 'express:sess.sig':'5rsqTabounLZ_yYBTYpBYv2moWk'}
 
 x = requests.post(url, data = myobj, cookies=head)
